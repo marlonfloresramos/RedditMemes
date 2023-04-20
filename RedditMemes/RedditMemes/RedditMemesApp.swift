65ae839580f96699f8efc1aa2fd5c2f9b7bed1a7
marlonfr <marlon.floresramos07@gmail.com>
2023-04-20 10:02:39 -0500

    Project creation and set initial configuration

---
+++ RedditMemes/RedditMemes/RedditMemesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditMemesApp.swift
+//  RedditMemes
+//
+//  Created by Marlon Gabriel Flores Ramos on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RedditMemesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
