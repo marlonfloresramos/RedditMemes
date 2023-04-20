d2b1873c3c057cdd9c33936bba4e3a0e82092c2b
marlonfr <marlon.floresramos07@gmail.com>
2023-04-20 11:22:03 -0500

    add app delegate

---
+++ RedditMemes/RedditMemes/Application/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  RedditMemes
+//
+//  Created by Marlon Gabriel Flores Ramos on 20/04/23.
+//
+
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        return true
+    }
+}
